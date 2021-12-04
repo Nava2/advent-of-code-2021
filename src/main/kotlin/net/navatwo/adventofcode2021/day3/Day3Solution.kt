fd35e3a6d23a9d67b4ff4e98bc2218d1177cce25
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-04 14:25:17 -0500

    Add data

---
+++ src/main/kotlin/net/navatwo/adventofcode2021/day3/Day3Solution.kt
@@ -0,0 +1,15 @@
+package net.navatwo.adventofcode2021.day3
+
+import net.navatwo.adventofcode2021.framework.Solution
+
+sealed class Day3Solution : Solution<Int, Int> {
+    object Part1 : Day3Solution() {
+        override fun solve(inputs: List<Int>): Int {
+            TODO()
+        }
+    }
+
+    override fun parse(lines: List<String>): List<Int> {
+        TODO()
+    }
+}
