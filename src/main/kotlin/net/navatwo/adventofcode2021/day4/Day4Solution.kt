033e41d28bdef6e93361f30836d8129e8d149d19
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-05 09:06:40 -0500

    loading done

--- src/main/kotlin/net/navatwo/adventofcode2021/day4/Day4Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2021/day4/Day4Solution.kt
@@ -2,6 +2,8 @@ package net.navatwo.adventofcode2021.day4
 
 import net.navatwo.adventofcode2021.framework.Solution
 
+private val CARD_ROWS = 5
+
 sealed class Day4Solution<S> : Solution<Day4Solution.Input, S> {
     object Part1 : Day4Solution<Int>() {
         override fun solve(input: Input): Int {
@@ -10,8 +12,35 @@ sealed class Day4Solution<S> : Solution<Day4Solution.Input, S> {
     }
 
     override fun parse(lines: List<String>): Input {
-        TODO()
+        val numbersLine = lines[0]
+        val firstCardIndex = 2
+
+        val numbers = numbersLine.splitToSequence(',')
+            .map { it.toInt() }
+            .toList()
+        val cards = (firstCardIndex..lines.size step (CARD_ROWS + 1)).map { firstLineIndex ->
+            val cardLines = lines.subList(firstLineIndex, firstLineIndex + CARD_ROWS)
+            val board = cardLines.map { line ->
+                line.splitToSequence(' ')
+                    .filter { it.isNotBlank() }
+                    .map { it.toInt() }
+                    .map { Card.Element(it, false) }
+                    .toList()
+            }
+            Card(board)
+        }
+
+        return Input(numbers = numbers, cards = cards)
     }
 
-    data class Input(val int: Int)
+    data class Input(
+        val numbers: List<Int>,
+        val cards: List<Card>,
+    )
+
+    data class Card(
+        val board: List<List<Element>>,
+    ) {
+        data class Element(val value: Int, val picked: Boolean)
+    }
 }
--- src/test/kotlin/net/navatwo/adventofcode2021/Day4SolutionTest.kt
+++ src/test/kotlin/net/navatwo/adventofcode2021/Day4SolutionTest.kt
@@ -17,6 +17,33 @@ class Day4SolutionTest {
     fun `p1 sample`() {
         val input = part1.parseResource(SAMPLE_RESOURCE)
 
+        assertThat(input.numbers).startsWith(7, 4, 9, 5, 11)
+        assertThat(input.cards).hasSize(3)
+
+        assertThat(input.cards).containsExactly(
+            createCard(
+                listOf(22, 13, 17, 11, 0),
+                listOf(8, 2, 23, 4, 24),
+                listOf(21, 9, 14, 16, 7),
+                listOf(6, 10, 3, 18, 5),
+                listOf(1, 12, 20, 15, 19),
+            ),
+            createCard(
+                listOf(3, 15, 0, 2, 22),
+                listOf(9, 18, 13, 17, 5),
+                listOf(19, 8, 7, 25, 23),
+                listOf(20, 11, 10, 24, 4),
+                listOf(14, 21, 16, 12, 6),
+            ),
+            createCard(
+                listOf(14, 21, 17, 24, 4),
+                listOf(10, 16, 15, 9, 19),
+                listOf(18, 8, 23, 26, 20),
+                listOf(22, 11, 13, 6, 5),
+                listOf(2, 0, 12, 3, 7),
+            ),
+        )
+
         val result = part1.solve(input)
         assertThat(result).isEqualTo(198)
 
@@ -65,3 +92,7 @@ class Day4SolutionTest {
         )
     }
 }
+
+private fun createCard(vararg rows: List<Int>): Day4Solution.Card = Day4Solution.Card(
+    board = rows.map { row -> row.map { Day4Solution.Card.Element(it, false) } },
+)
