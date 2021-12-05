fca3c6c36bcb6d09b5678d10a2e092809a1bd31d
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-05 11:53:43 -0500

    Day4 (#2)

---
+++ src/main/kotlin/net/navatwo/adventofcode2021/day4/Day4Solution.kt
@@ -0,0 +1,187 @@
+package net.navatwo.adventofcode2021.day4
+
+import net.navatwo.adventofcode2021.framework.Solution
+import java.util.Objects
+
+private val CARD_ROWS = 5
+
+sealed class Day4Solution<S> : Solution<Day4Solution.Input, S> {
+    object Part1 : Day4Solution<Result>() {
+        override fun solve(input: Input): Result {
+            val numbers = input.numbers
+            val cards = input.cards.associateWith { Card.create(it) }
+
+            return numbers
+                .mapNotNull { pickedNumber ->
+                    val winningEntries = cards.filterValues { card -> card.pickValue(pickedNumber) }
+
+                    if (winningEntries.isEmpty()) return@mapNotNull null
+
+                    winningEntries.firstNotNullOf { (c, card) ->
+                        Result(
+                            number = pickedNumber,
+                            card = c,
+                            computed = card.result(pickedNumber),
+                        )
+                    }
+                }
+                .first()
+        }
+    }
+
+    object Part2 : Day4Solution<Result>() {
+        override fun solve(input: Input): Result {
+            val numbers = input.numbers
+            val cards = input.cards
+                .associateWith { Card.create(it) }
+                .toMutableMap()
+
+            return numbers
+                .mapNotNull { pickedNumber ->
+                    val winningCards = cards.entries.filter { (_, card) ->
+                        card.pickValue(pickedNumber)
+                    }
+
+                    if (winningCards.isEmpty()) {
+                        return@mapNotNull null
+                    }
+
+                    winningCards.firstNotNullOfOrNull { (c, card) ->
+                        if (cards.size == 1) {
+                            Result(
+                                number = pickedNumber,
+                                card = c,
+                                computed = card.result(pickedNumber),
+                            )
+                        } else {
+                            cards.remove(c)
+                            null
+                        }
+                    }
+                }
+                .first()
+        }
+    }
+
+    override fun parse(lines: List<String>): Input {
+        val numbersLine = lines[0]
+        val firstCardIndex = 2
+
+        val numbers = numbersLine.splitToSequence(',')
+            .map { it.toInt() }
+            .toList()
+        val cards = (firstCardIndex..lines.size step (CARD_ROWS + 1)).map { firstLineIndex ->
+            val cardLines = lines.subList(firstLineIndex, firstLineIndex + CARD_ROWS)
+            val board = cardLines.map { line ->
+                line.splitToSequence(' ')
+                    .filter { it.isNotBlank() }
+                    .map { it.toInt() }
+                    .toList()
+            }
+            Input.Card(board)
+        }
+
+        return Input(numbers = numbers, cards = cards)
+    }
+
+    data class Input(
+        val numbers: List<Int>,
+        val cards: List<Card>,
+    ) {
+        data class Card(
+            val board: List<List<Int>>,
+        )
+    }
+
+    data class Result(
+        val number: Int,
+        val card: Input.Card,
+        val computed: Int,
+    )
+
+    private class Card(
+        val board: List<List<Element>>,
+        private val elementsByNumber: Map<Int, Element>,
+    ) {
+        private val valueLocations: Map<Int, Pair<Int, Int>>
+
+        init {
+            val valueLocations = HashMap<Int, Pair<Int, Int>>(elementsByNumber.size)
+            for ((r, row) in board.withIndex()) {
+                for ((c, value) in row.withIndex()) {
+                    valueLocations[value.value] = r to c
+                }
+            }
+
+            this.valueLocations = valueLocations
+        }
+
+        private val columnCount = MutableList(CARD_ROWS) { 0 }
+        private val rowCount = MutableList(CARD_ROWS) { 0 }
+
+        fun pickValue(value: Int): Boolean {
+            val (row, column) = valueLocations[value] ?: return false
+
+            val element = board[row][column]
+            if (!element.picked) {
+                element.picked = true
+                columnCount[column] += 1
+                rowCount[row] += 1
+            }
+
+            return columnCount[column] == CARD_ROWS || rowCount[row] == CARD_ROWS
+        }
+
+        fun result(number: Int): Int {
+            val unmarkedSum = board.sumOf { row ->
+                row.asSequence().filterNot { it.picked }.sumOf { it.value }
+            }
+
+            return unmarkedSum * number
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (other !is Card) return false
+            return board == other.board
+        }
+
+        override fun hashCode(): Int {
+            return Objects.hash(board)
+        }
+
+        class Element(val value: Int, var picked: Boolean) {
+            override fun equals(other: Any?): Boolean {
+                if (other !is Element) return false
+                return value == other.value
+            }
+
+            override fun hashCode(): Int {
+                return Objects.hash(value)
+            }
+
+            override fun toString(): String {
+                return (if (picked) "*$value*" else value.toString()).padStart(4, ' ')
+            }
+        }
+
+        companion object {
+            fun create(card: Input.Card): Card {
+                val elementBoard = ArrayList<ArrayList<Element>>(CARD_ROWS)
+                val elementsByNumber = HashMap<Int, Element>(CARD_ROWS * CARD_ROWS)
+
+                for (row in card.board) {
+                    val elementRow = ArrayList<Element>(CARD_ROWS)
+                    elementBoard.add(elementRow)
+
+                    for (value in row) {
+                        val element = Element(value, false)
+                        elementRow.add(element)
+                        elementsByNumber[value] = element
+                    }
+                }
+
+                return Card(board = elementBoard, elementsByNumber = elementsByNumber)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/net/navatwo/adventofcode2021/Day4SolutionTest.kt
@@ -0,0 +1,87 @@
+package net.navatwo.adventofcode2021
+
+import net.navatwo.adventofcode2021.benchmarks.Benchmark
+import net.navatwo.adventofcode2021.day4.Day4Solution
+import net.navatwo.adventofcode2021.day4.Day4Solution.Input
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+private const val SAMPLE_RESOURCE = "day4/p1_sample.txt"
+private const val INPUT_RESOURCE = "day4/p1_input.txt"
+private val part1 = Day4Solution.Part1
+private val part2 = Day4Solution.Part2
+
+class Day4SolutionTest {
+    @Test
+    fun `p1 sample`() {
+        val input = part1.parseResource(SAMPLE_RESOURCE)
+
+        assertThat(input.numbers).startsWith(7, 4, 9, 5, 11)
+        assertThat(input.cards).hasSize(3)
+
+        assertThat(input.cards).containsExactly(
+            createCard(
+                listOf(22, 13, 17, 11, 0),
+                listOf(8, 2, 23, 4, 24),
+                listOf(21, 9, 14, 16, 7),
+                listOf(6, 10, 3, 18, 5),
+                listOf(1, 12, 20, 15, 19),
+            ),
+            createCard(
+                listOf(3, 15, 0, 2, 22),
+                listOf(9, 18, 13, 17, 5),
+                listOf(19, 8, 7, 25, 23),
+                listOf(20, 11, 10, 24, 4),
+                listOf(14, 21, 16, 12, 6),
+            ),
+            createCard(
+                listOf(14, 21, 17, 24, 4),
+                listOf(10, 16, 15, 9, 19),
+                listOf(18, 8, 23, 26, 20),
+                listOf(22, 11, 13, 6, 5),
+                listOf(2, 0, 12, 3, 7),
+            ),
+        )
+
+        val result = part1.solve(input)
+        assertThat(result.computed).isEqualTo(4512)
+    }
+
+    @Test
+    fun p1() {
+        val input = part1.parseResource(INPUT_RESOURCE)
+
+        val result = part1.solve(input)
+        assertThat(result.computed).isEqualTo(22680)
+
+        Benchmark.run(
+            inputContent = loadLines(INPUT_RESOURCE),
+            solution = part1,
+        )
+    }
+
+    @Test
+    fun `p2 - sample`() {
+        val input = part2.parseResource(SAMPLE_RESOURCE)
+
+        val result = part2.solve(input)
+        assertThat(result.computed).isEqualTo(1924)
+    }
+
+    @Test
+    fun p2() {
+        val input = part2.parseResource(INPUT_RESOURCE)
+
+        val result = part2.solve(input)
+        assertThat(result.computed).isEqualTo(16168)
+
+        Benchmark.run(
+            inputContent = loadLines(INPUT_RESOURCE),
+            solution = part2,
+        )
+    }
+}
+
+private fun createCard(vararg rows: List<Int>): Input.Card = Input.Card(
+    board = rows.toList(),
+)
