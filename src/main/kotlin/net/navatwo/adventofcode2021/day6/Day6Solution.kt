82b49c00f11eeec4fc88110fd18812b15c915940
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-06 21:11:47 -0500

    Part 1 and 2

--- src/main/kotlin/net/navatwo/adventofcode2021/day6/Day6Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2021/day6/Day6Solution.kt
@@ -1,12 +1,21 @@
 package net.navatwo.adventofcode2021.day6
 
-import net.navatwo.adventofcode2021.day6.Day6Solution.Input.AnglerFish
+import net.navatwo.adventofcode2021.day6.Day6Solution.Result.FishTable
 import net.navatwo.adventofcode2021.framework.Solution
+
+const val NEW_BIRTH_DAYS = 8
+const val BUFFER_COUNT = NEW_BIRTH_DAYS + 1
 
 sealed class Day6Solution : Solution<Day6Solution.Input, Day6Solution.Result> {
     object Part1 : Day6Solution() {
         override fun solve(input: Input): Result {
-            TODO()
+            return computeFish(input, 80)
+        }
+    }
+
+    object Part2 : Day6Solution() {
+        override fun solve(input: Input): Result {
+            return computeFish(input, 256)
         }
     }
 
@@ -18,14 +27,74 @@ sealed class Day6Solution : Solution<Day6Solution.Input, Day6Solution.Result> {
         )
     }
 
+    @JvmInline
+    value class AnglerFish(val timeToSpawn: Int)
+
     data class Input(
         val fish: List<AnglerFish>,
-    ) {
-        @JvmInline
-        value class AnglerFish(val timeToSpawn: Int)
-    }
+    )
 
     data class Result(
-        val computed: Int,
-    )
+        val fishTable: FishTable,
+        val computed: Long,
+    ) {
+        data class FishTable(
+            val fish: List<Long>,
+        ) {
+            fun fishes(): Iterable<AnglerFish> {
+                return fish.asSequence()
+                    .withIndex()
+                    .flatMap { (timeToSpawn, count) ->
+                        (0 until count).asSequence().map { AnglerFish(timeToSpawn) }
+                    }
+                    .asIterable()
+            }
+
+            constructor(
+                days: Int,
+                fish: LongArray,
+            ) : this(
+                (days until days + BUFFER_COUNT).map { fish[it % BUFFER_COUNT] }
+            )
+        }
+    }
+
+    companion object {
+
+        internal fun computeFish(input: Input, days: Int): Result {
+            // each index is a day offset
+            // each value is a count of fish
+            val fish = LongArray(BUFFER_COUNT)
+            for (f in input.fish) {
+                fish[f.timeToSpawn] += 1L
+            }
+
+//            run {
+//                val fishTable = FishTable(0, fish)
+//                println("Initial state: " +
+//                        fishTable.fishes().joinToString(",") { it.timeToSpawn.toString() })
+//            }
+
+            for (day in 0 until days) {
+                val index = day % BUFFER_COUNT // index in the array
+                val spawningToday = fish[index]
+                fish[index] = 0
+
+                val respawnIndex = (day + NEW_BIRTH_DAYS - 1) % BUFFER_COUNT
+                fish[respawnIndex] += spawningToday
+
+                val newSpawnIndex = (day + NEW_BIRTH_DAYS + 1) % BUFFER_COUNT
+                fish[newSpawnIndex] += spawningToday
+
+//                val fishTable = FishTable(day + 1, fish)
+//                println("After ${(day + 1).toString().padStart(2, ' ')} days: " +
+//                        fishTable.fishes().joinToString(",") { it.timeToSpawn.toString() })
+            }
+
+            return Result(
+                fishTable = FishTable(days, fish),
+                computed = fish.sumOf { it },
+            )
+        }
+    }
 }
--- src/test/kotlin/net/navatwo/adventofcode2021/Day6SolutionTest.kt
+++ src/test/kotlin/net/navatwo/adventofcode2021/Day6SolutionTest.kt
@@ -2,14 +2,15 @@ package net.navatwo.adventofcode2021
 
 import net.navatwo.adventofcode2021.benchmarks.Benchmark
 import net.navatwo.adventofcode2021.day6.Day6Solution
-import net.navatwo.adventofcode2021.day6.Day6Solution.Input.AnglerFish
+import net.navatwo.adventofcode2021.day6.Day6Solution.AnglerFish
+import net.navatwo.adventofcode2021.day6.Day6Solution.Input
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 private const val SAMPLE_RESOURCE = "day6/p1_sample.txt"
 private const val INPUT_RESOURCE = "day6/p1_input.txt"
 private val part1 = Day6Solution.Part1
-private val part2 = Day6Solution.Part1
+private val part2 = Day6Solution.Part2
 
 class Day6SolutionTest {
     @Test
@@ -24,8 +25,26 @@ class Day6SolutionTest {
             AnglerFish(2),
         )
 
+        runAndAssert(
+            input = input,
+            days = 2,
+            expected = listOf(1, 2, 1, 6, 0, 8),
+        )
+
+        runAndAssert(
+            input = input,
+            days = 3,
+            expected = listOf(0, 1, 0, 5, 6, 7, 8),
+        )
+
+        runAndAssert(
+            input = input,
+            days = 18,
+            expected = listOf(6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8),
+        )
+
         val result = part1.solve(input)
-        assertThat(result.computed).isEqualTo(5)
+        assertThat(result.computed).isEqualTo(5934)
     }
 
     @Test
@@ -33,7 +52,7 @@ class Day6SolutionTest {
         val input = part1.parseResource(INPUT_RESOURCE)
 
         val result = part1.solve(input)
-        assertThat(result.computed).isEqualTo(5774)
+        assertThat(result.computed).isEqualTo(363101)
 
         Benchmark.run(
             inputContent = loadLines(INPUT_RESOURCE),
@@ -46,7 +65,7 @@ class Day6SolutionTest {
         val input = part2.parseResource(SAMPLE_RESOURCE)
 
         val result = part2.solve(input)
-        assertThat(result.computed).isEqualTo(12)
+        assertThat(result.computed).isEqualTo(26984457539L)
     }
 
     @Test
@@ -54,11 +73,20 @@ class Day6SolutionTest {
         val input = part2.parseResource(INPUT_RESOURCE)
 
         val result = part2.solve(input)
-        assertThat(result.computed).isEqualTo(18423)
+        assertThat(result.computed).isEqualTo(1644286074024L)
 
         Benchmark.run(
             inputContent = loadLines(INPUT_RESOURCE),
             solution = part2,
         )
+    }
+
+    private fun runAndAssert(input: Input, days: Int, expected: Collection<Int>) {
+        val result = Day6Solution.computeFish(input, days)
+
+        assertThat(result.fishTable.fishes()).containsExactlyInAnyOrderElementsOf(
+            expected.sorted().map { AnglerFish(it) },
+        )
+        assertThat(result.computed).isEqualTo(expected.size.toLong())
     }
 }
