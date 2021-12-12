a570c740cbef062a53ec9adae766b4a0798f5b8d
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-11 20:33:08 -0500

    Part 1

--- src/main/kotlin/net/navatwo/adventofcode2021/day9/Day9Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2021/day9/Day9Solution.kt
@@ -1,19 +1,35 @@
 package net.navatwo.adventofcode2021.day9
 
-import net.navatwo.adventofcode2021.day8.Signal.A
-import net.navatwo.adventofcode2021.day8.Signal.B
-import net.navatwo.adventofcode2021.day8.Signal.C
-import net.navatwo.adventofcode2021.day8.Signal.D
-import net.navatwo.adventofcode2021.day8.Signal.E
-import net.navatwo.adventofcode2021.day8.Signal.F
-import net.navatwo.adventofcode2021.day8.Signal.G
 import net.navatwo.adventofcode2021.framework.ComputedResult
 import net.navatwo.adventofcode2021.framework.Solution
 
 sealed class Day9Solution : Solution<Day9Solution.Input> {
     object Part1 : Day9Solution() {
         override fun solve(input: Input): ComputedResult {
-            return ComputedResult.Simple(10)
+            val heightMap = input.heightMap
+
+            val totalRisk = heightMap.asSequence()
+                .withIndex()
+                .flatMap { (y, row) ->
+                    row.asSequence()
+                        .withIndex()
+                        .mapNotNull { (x, cell) ->
+                            val isLower = listOfNotNull(
+                                input.getCoord(x - 1, y),
+                                input.getCoord(x, y - 1),
+                                input.getCoord(x + 1, y),
+                                input.getCoord(x, y + 1),
+                            ).all { it.height > cell.height }
+
+                            (y to x).takeIf { isLower }
+                        }
+                }
+                .mapNotNull { (y, x) ->
+                    input.getCoord(x, y)?.let { it.height + 1 }
+                }
+                .sum()
+
+            return ComputedResult.Simple(totalRisk)
         }
     }
 
@@ -24,10 +40,24 @@ sealed class Day9Solution : Solution<Day9Solution.Input> {
     }
 
     override fun parse(lines: List<String>): Input {
-        TODO("oof")
+        return Input(
+            heightMap = lines.map { line ->
+                line.map { c ->
+                    Height(c.digitToInt())
+                }
+            }
+        )
     }
 
     data class Input(
-        val numbers: List<Int>,
+        val heightMap: List<List<Height>>,
     )
+
+    fun Input.getCoord(x: Int, y: Int): Height? {
+        val row = heightMap.getOrNull(y)
+        return row?.getOrNull(x)
+    }
+
+    @JvmInline
+    value class Height(val height: Int)
 }
