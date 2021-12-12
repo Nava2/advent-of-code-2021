3c47035cd2209168debcb9ab0fae3592d5cc004e
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-12 10:18:43 -0500

    Day9 (#7)

---
+++ src/main/kotlin/net/navatwo/adventofcode2021/day9/Day9Solution.kt
@@ -0,0 +1,134 @@
+package net.navatwo.adventofcode2021.day9
+
+import net.navatwo.adventofcode2021.framework.ComputedResult
+import net.navatwo.adventofcode2021.framework.Solution
+
+sealed class Day9Solution : Solution<Day9Solution.Input> {
+    object Part1 : Day9Solution() {
+        override fun solve(input: Input): ComputedResult {
+            val totalRisk = input.lowPoints()
+                .mapNotNull { (y, x) ->
+                    input.heightMap.getCoord(x, y)?.let { it.height + 1 }
+                }
+                .sum()
+
+            return ComputedResult.Simple(totalRisk)
+        }
+    }
+
+    object Part2 : Day9Solution() {
+        override fun solve(input: Input): ComputedResult {
+            val visited = input.initializeVisited()
+
+            val basins = input.lowPoints()
+                .map { coord ->
+                    val queue = ArrayDeque<Coord>()
+                    queue.addFirst(coord)
+
+                    var basinSize = 0
+
+                    while (queue.isNotEmpty()) {
+                        val toCheck = queue.removeFirst()
+                        val (x, y) = toCheck
+
+                        if (visited[y][x]) {
+                            continue
+                        }
+
+                        basinSize += 1
+                        visited[y][x] = true
+
+                        val tileHeight = input.heightMap.getCoord(x, y)!!.height
+
+                        queue.addAll(
+                            forCoords(x, y).filter { (x, y) ->
+                                val tile = input.heightMap.getCoord(x, y)
+                                    ?: return@filter false
+
+                                if (visited[y][x]) return@filter false
+
+                                tile.height != 9 && tile.height > tileHeight
+                            }
+                        )
+                    }
+
+                    coord to basinSize
+                }
+
+
+            val sortedBasins = basins.sortedByDescending { (_, counter) -> counter }
+
+            val result = sortedBasins[0].second *
+                    sortedBasins[1].second *
+                    sortedBasins[2].second
+
+            return ComputedResult.Simple(result)
+        }
+
+
+        private fun Input.initializeVisited(): Array<BooleanArray> {
+            val columnCount = heightMap.first().size
+            return Array(heightMap.size) { BooleanArray(columnCount) }
+        }
+    }
+
+    override fun parse(lines: List<String>): Input {
+        return Input(
+            heightMap = lines.map { line ->
+                line.map { c ->
+                    Height(c.digitToInt())
+                }
+            }
+        )
+    }
+
+    internal fun forCoords(x: Int, y: Int): Iterable<Coord> = listOf(
+        Coord(x - 1, y),
+        Coord(x, y - 1),
+        Coord(x + 1, y),
+        Coord(x, y + 1),
+    )
+
+    internal fun Input.lowPoints(): Iterable<Coord> {
+        return heightMap.asSequence()
+            .withIndex()
+            .flatMap { (y, row) ->
+                row.asSequence()
+                    .withIndex()
+                    .mapNotNull { (x, cell) ->
+                        val isLower = forCoords(x, y).all {
+                            val height = heightMap.getCoord(it)?.height
+                            height != null && height > cell.height
+                        }
+
+                        Coord(x, y).takeIf { isLower }
+                    }
+            }
+            .toList()
+    }
+
+    data class Input(
+        val heightMap: List<List<Height>>,
+    )
+
+    fun Input.isValidCoord(x: Int, y: Int): Boolean {
+        return y in heightMap.indices &&
+                x in heightMap.first().indices
+    }
+
+    fun Input.isValidCoord(coord: Coord): Boolean = isValidCoord(coord.x, coord.y)
+
+    @JvmInline
+    value class Height(val height: Int)
+}
+
+fun <T> List<List<T>>.getCoord(x: Int, y: Int): T? {
+    val row = getOrNull(y)
+    return row?.getOrNull(x)
+}
+
+fun <T> List<List<T>>.getCoord(coord: Coord): T? {
+    return getCoord(coord.x, coord.y)
+}
+
+data class Coord(val x: Int, val y: Int)
