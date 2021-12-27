3e06d1fb6fe6a04d86b9ac69ed8212ac8720e6fb
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-27 07:40:50 -0500

    Priority queue

--- src/main/kotlin/net/navatwo/adventofcode2021/day15/Day15Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2021/day15/Day15Solution.kt
@@ -5,6 +5,7 @@ import net.navatwo.adventofcode2021.framework.ComputedResult
 import net.navatwo.adventofcode2021.framework.Solution
 import net.navatwo.adventofcode2021.get
 import net.navatwo.adventofcode2021.set
+import java.util.PriorityQueue
 
 sealed class Day15Solution : Solution<Day15Solution.Input> {
     object Part1 : Day15Solution() {
@@ -16,39 +17,54 @@ sealed class Day15Solution : Solution<Day15Solution.Input> {
                 BooleanArray(rowSize)
             }
 
-            val costs = Array(board.size) {
+            val dists = Array(board.size) {
                 IntArray(rowSize) { Int.MAX_VALUE }
             }
 
             val start = Coord(0, 0)
+            dists[start] = 0
+
             val end = Coord(board.first().lastIndex, board.lastIndex)
 
-            val queue = ArrayDeque<Coord>()
-            queue.add(start)
+            val queue = PriorityQueue<Coord>(compareBy { dists[it] })
+
+            for (y in dists.indices) {
+                for (x in dists.first().indices) {
+                    val v = Coord(x, y)
+                    if (v != start) {
+                        dists[v] = Int.MAX_VALUE
+                    }
+
+                    queue.add(v)
+                }
+            }
 
             val prevMap = mutableMapOf<Coord, Coord>()
 
             while (queue.isNotEmpty()) {
-                val current = queue.removeFirst()
+                val current = queue.remove()
 
                 val visited = visits[current]
                 if (visited) continue
 
                 val neighbours = current.nextCoords()
-                    .filter { it.x >= 0 && it.y >= 0 && it.x <= costs.first().lastIndex && it.y <= costs.lastIndex }
+                    .filter { it.x >= 0 && it.y >= 0 && it.x <= dists.first().lastIndex && it.y <= dists.lastIndex }
                     .filter { !visits[it] }
 
-                val currentCost = costs[current]
+                val currentCost = dists[current]
                 for (neighbour in neighbours) {
                     val altCost = currentCost + board[neighbour].risk
-                    if (altCost < costs[neighbour]) {
-                        costs[neighbour] = altCost
+                    if (altCost < dists[neighbour]) {
+                        dists[neighbour] = altCost
                         prevMap[neighbour] = current
+
+                        // update the priority
+                        queue.remove(neighbour)
+                        queue.add(neighbour)
                     }
                 }
 
                 visits[current] = true
-                queue.addAll(neighbours)
 
                 if (current == end) break
             }
