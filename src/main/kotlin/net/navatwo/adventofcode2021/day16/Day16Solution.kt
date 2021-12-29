412b1347b055fefa63411c777d378266db9299eb
Kevin Brightwell <kevin.brightwell@faire.com>
2021-12-29 12:02:40 -0500

    Day 16 part 1 and 2

--- src/main/kotlin/net/navatwo/adventofcode2021/day16/Day16Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2021/day16/Day16Solution.kt
@@ -6,13 +6,15 @@ import net.navatwo.adventofcode2021.framework.Solution
 sealed class Day16Solution : Solution<Day16Solution.Input> {
     object Part1 : Day16Solution() {
         override fun solve(input: Input): ComputedResult {
-            TODO("Not yet implemented")
+            val packet = Packet.parse(input.bits)
+            return ComputedResult.Simple(packet.sumVersions())
         }
     }
 
     object Part2 : Day16Solution() {
         override fun solve(input: Input): ComputedResult {
-            TODO("Not yet implemented")
+            val rootPacket = Packet.parse(input.bits)
+            return ComputedResult.Simple(rootPacket.calculate())
         }
     }
 
@@ -24,25 +26,93 @@ sealed class Day16Solution : Solution<Day16Solution.Input> {
                 .map { it == '1' }
         }
 
-        return Input(Packet.parse(bits))
+        return Input(bits)
     }
 
     data class Input(
-        val packets: List<Packet>,
+        val bits: List<Boolean>,
     )
 
-    sealed interface Packet {
+    sealed class Packet {
 
-        val version: Int
-        val typeId: TypeId
+        abstract val version: Int
+        abstract val typeId: TypeId
 
-        enum class TypeId(val id: Int) {
-            LITERAL(4),
+        abstract fun sumVersions(): Long
+
+        abstract fun calculate(): Long
+
+        enum class TypeId(val value: Int, val isOperator: Boolean) {
+            SUM(value = 0, isOperator = true),
+            PRODUCT(value = 1, isOperator = true),
+            MINIMUM(value = 2, isOperator = true),
+            MAXIMUM(value = 3, isOperator = true),
+            LITERAL(value = 4, isOperator = false),
+            GREATER_THAN(value = 5, isOperator = true),
+            LESS_THAN(value = 6, isOperator = true),
+            EQUAL_TO(value = 7, isOperator = true),
             ;
 
             companion object {
-                fun valueOf(id: Int): TypeId {
-                    return values().single { it.id == id }
+                private val byValue: Map<Int, TypeId> = values().associateBy { it.value }
+
+                fun valueOf(id: Int): TypeId = byValue.getValue(id)
+            }
+        }
+
+        data class Operator(
+            override val version: Int,
+            override val typeId: TypeId,
+            val packets: List<Packet>,
+        ) : Packet() {
+            override fun sumVersions(): Long = version + packets.sumOf { it.sumVersions() }
+
+
+            override fun calculate(): Long = when (typeId) {
+                TypeId.SUM -> packets.sumOf { it.calculate() }
+                TypeId.PRODUCT -> packets.fold(1L) { acc, packet -> acc * packet.calculate() }
+                TypeId.MINIMUM -> packets.minOf { it.calculate() }
+                TypeId.MAXIMUM -> packets.maxOf { it.calculate() }
+
+                // binary operations
+                TypeId.GREATER_THAN -> {
+                    val (a, b) = forBinaryOperation()
+                    if (a > b) 1L else 0L
+                }
+                TypeId.LESS_THAN -> {
+                    val (a, b) = forBinaryOperation()
+                    if (a < b) 1L else 0L
+                }
+                TypeId.EQUAL_TO -> {
+                    val (a, b) = forBinaryOperation()
+                    if (a == b) 1L else 0L
+                }
+
+                else -> error("unknown operation")
+            }
+
+            private fun forBinaryOperation(): Pair<Long, Long> {
+                val values = packets.map { it.calculate() }
+                check(values.size == 2) {
+                    "values.size > 2 (n = ${values.size})"
+                }
+
+                val (a, b) = values
+                return a to b
+            }
+
+            companion object {
+                fun parseOperator(version: Int, typeId: TypeId, buffer: ConsumableBuffer): Operator? {
+                    val operatorTypeFlag = buffer.tryConsume(1)?.single() ?: return null
+                    val subpackets = if (!operatorTypeFlag) {
+                        val lengthOfSubpackets = buffer.tryConsumeInt(15) ?: return null
+                        parse(buffer.consumeAsBuffer(lengthOfSubpackets), maxPackets = Int.MAX_VALUE)
+                    } else {
+                        val numberOfSubpackets = buffer.tryConsumeInt(11) ?: return null
+                        parse(buffer, maxPackets = numberOfSubpackets)
+                    }
+
+                    return Operator(version, typeId, subpackets)
                 }
             }
         }
@@ -50,80 +120,126 @@ sealed class Day16Solution : Solution<Day16Solution.Input> {
         data class Literal(
             override val version: Int,
             override val typeId: TypeId,
-            val literals: List<Int>
-        ) : Packet {
+            val literal: Long,
+        ) : Packet() {
+            override fun sumVersions(): Long = version.toLong()
+
+            override fun calculate(): Long = literal
+
             companion object {
-                fun parseLiterals(version: Int, typeId: TypeId, buffer: ConsumableBuffer): Literal {
-                    val literals = mutableListOf<Int>()
-                    do {
-                        val isLast = buffer.consumeInt(1) == 0
-                        val elements = buffer.consumeInt(4)
-                        literals.add(elements)
-                    } while (buffer.hasNext() && !isLast)
+                fun parseLiterals(
+                    version: Int,
+                    typeId: TypeId,
+                    buffer: ConsumableBuffer,
+                ): Literal {
+                    val literals = sequence {
+                        do {
+                            val isLast = buffer.tryConsumeInt(1) ?: break
+                            val values = buffer.tryConsume(4) ?: break
 
-                    buffer.takeUntil(3) { it } // take until there's a non-zero
+                            yieldAll(values)
+                        } while (buffer.hasNext() && isLast == 1)
+                    }
 
-                    return Literal(version, typeId, literals)
+                    return Literal(version, typeId, literal = literals.asIterable().toLong())
                 }
-            }
-        }
-
-        class ConsumableBuffer(private val buffer: List<Boolean>) {
-            private var index = 0
-
-            fun hasNext(): Boolean = index <= buffer.lastIndex
-
-            fun consumeInt(n: Int): Int {
-                val result = buffer.subList(index, index + n)
-                index += n
-                return result.toInt()
-            }
-
-            fun takeUntil(maxIterations: Int, predicate: (Boolean) -> Boolean): Iterable<Boolean> {
-                var iterIndex = index
-                while (
-                    iterIndex <= buffer.lastIndex &&
-                    iterIndex - index < maxIterations &&
-                    !predicate(buffer[iterIndex])
-                ) {
-                    iterIndex += 1
-                }
-
-                val result = buffer.subList(index, iterIndex)
-                index = iterIndex
-                return result
             }
         }
 
         companion object {
 
-            fun parse(bits: List<Boolean>): List<Packet> {
-                val buffer = ConsumableBuffer(bits)
+            fun parse(bits: List<Boolean>): Packet {
+                return parseSingle(ConsumableBuffer(bits))!!
+            }
 
+            private fun parse(buffer: ConsumableBuffer, maxPackets: Int): List<Packet> {
                 val results = mutableListOf<Packet>()
-                while (buffer.hasNext()) {
-                    val version = buffer.consumeInt(3)
-
-                    val packet = when (val typeId = TypeId.valueOf(buffer.consumeInt(3))) {
-                        TypeId.LITERAL -> Literal.parseLiterals(version, typeId, buffer)
-                    }
-
+                while (buffer.hasNext() && results.size < maxPackets) {
+                    val packet = parseSingle(buffer) ?: break
                     results.add(packet)
+                }
+
+                if (maxPackets != Int.MAX_VALUE) {
+                    check(results.size == maxPackets) {
+                        "Failed to read $maxPackets size (size = ${results.size})"
+                    }
                 }
 
                 return results
             }
 
-            private fun Iterable<Boolean>.toInt(): Int {
-                return fold(0) { acc, v -> (acc shl 1) or (if (v) 1 else 0) }
-            }
+            private fun parseSingle(buffer: ConsumableBuffer): Packet? {
+                val version = buffer.tryConsume(3)?.toInt() ?: return null
 
-            private fun <T> Iterator<T>.take(n: Int): List<T> {
-                return List(n) {
-                    check(hasNext()) { "No values remain" }
-                    next()
+                val typeId = buffer.tryConsumeInt(3)?.let { TypeId.valueOf(it) } ?: return null
+
+                return if (typeId.isOperator) {
+                    Operator.parseOperator(version, typeId, buffer)
+                } else {
+                    Literal.parseLiterals(
+                        version = version,
+                        typeId = typeId,
+                        buffer = buffer,
+                    )
                 }
             }
         }
     }
+
+    class ConsumableBuffer(private val buffer: List<Boolean>) {
+        private var index = 0
+
+        fun hasNext(): Boolean = index <= buffer.lastIndex
+
+        fun consumeAsBuffer(n: Int): ConsumableBuffer = ConsumableBuffer(consume(n))
+
+        fun tryConsume(n: Int): List<Boolean>? {
+            if (n == 0) return listOf()
+
+            return if (index + n <= buffer.size) {
+                consume(n)
+            } else {
+                null
+            }
+        }
+
+        fun consume(n: Int): List<Boolean> {
+            require(index + n <= buffer.size) { "not enough bits available n=$n" }
+
+            if (n == 0) return listOf()
+
+            val result = buffer.subList(index, index + n)
+            index += n
+            return result
+        }
+
+        fun consumeInt(n: Int): Int {
+            require(n <= 32) { "Requested too many bits as Int" }
+            return consume(n).toInt()
+        }
+
+        fun tryConsumeInt(n: Int): Int? {
+            require(n <= 32) { "Requested too many bits as Int" }
+            return tryConsume(n)?.toInt()
+        }
+
+        override fun toString(): String {
+            return "Buffer{index=$index, length=${buffer.size}}"
+        }
+    }
+}
+
+private fun <T> Iterator<T>.take(n: Int): List<T> {
+    return List(n) {
+        check(hasNext()) { "No values remain" }
+        next()
+    }
+}
+
+private fun Iterable<Boolean>.toInt(): Int {
+    return fold(0) { acc, v -> (acc shl 1) or (if (v) 1 else 0) }
+}
+
+private fun Iterable<Boolean>.toLong(): Long {
+    return fold(0L) { acc, v -> (acc shl 1) or (if (v) 1L else 0L) }
 }
--- src/test/kotlin/net/navatwo/adventofcode2021/Day16SolutionTest.kt
+++ src/test/kotlin/net/navatwo/adventofcode2021/Day16SolutionTest.kt
@@ -1,14 +1,17 @@
 package net.navatwo.adventofcode2021
 
 import net.navatwo.adventofcode2021.benchmarks.Benchmark
-import net.navatwo.adventofcode2021.day15.Day15Solution
 import net.navatwo.adventofcode2021.day16.Day16Solution
 import net.navatwo.adventofcode2021.day16.Day16Solution.Packet
-import net.navatwo.adventofcode2021.day16.Day16Solution.Packet.TypeId.*
+import net.navatwo.adventofcode2021.day16.Day16Solution.Packet.TypeId.LESS_THAN
+import net.navatwo.adventofcode2021.day16.Day16Solution.Packet.TypeId.LITERAL
+import net.navatwo.adventofcode2021.day16.Day16Solution.Packet.TypeId.MAXIMUM
+import net.navatwo.adventofcode2021.day16.Day16Solution.Packet.TypeId.SUM
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.ObjectAssert
 import org.junit.jupiter.api.Test
+import java.util.function.Consumer
 
-private const val SAMPLE_RESOURCE = "day16/p1_sample.txt"
 private const val INPUT_RESOURCE = "day16/p1_input.txt"
 private val part1 = Day16Solution.Part1
 private val part2 = Day16Solution.Part2
@@ -16,27 +19,186 @@ private val part2 = Day16Solution.Part2
 class Day16SolutionTest {
     @Test
     fun `p1 sample - 1`() {
-        val input = part1.parse(listOf("D2FE28"))
-
-        assertThat(input.packets).containsExactly(
+        val literalInput = part1.parse(listOf("D2FE28"))
+        val packet = Packet.parse(literalInput.bits)
+        assertThat(packet.sumVersions()).isEqualTo(6)
+        assertThat(packet).matchesPacket(
             Packet.Literal(
                 version = 6,
                 typeId = LITERAL,
-                literals = listOf(
-                    7, // 0111
-                    14, // 1110
-                    5, // 0101
-                ),
+                literal = 2021L,
             ),
         )
     }
 
     @Test
-    fun `p1 sample`() {
-        val input = part1.parseResource(SAMPLE_RESOURCE)
+    fun `p1 sample - 2`() {
+        val operatorInput0 = part1.parse(listOf("38006F45291200"))
+        val packet = Packet.parse(operatorInput0.bits)
+        assertThat(packet.sumVersions()).isEqualTo(9)
 
-        val result = part1.solve(input)
-        assertThat(result).isComputed(40)
+        assertThat(packet).matchesPacket(
+            Packet.Operator(
+                version = 1,
+                typeId = LESS_THAN,
+                packets = listOf(
+                    Packet.Literal(
+                        version = 6,
+                        typeId = LITERAL,
+                        literal = 10L,
+                    ),
+                    Packet.Literal(
+                        version = 2,
+                        typeId = LITERAL,
+                        literal = 20L,
+                    ),
+                )
+            ),
+        )
+    }
+
+    @Test
+    fun `p1 sample - 3`() {
+        val operatorInput1 = part1.parse(listOf("EE00D40C823060"))
+
+        val packet = Packet.parse(operatorInput1.bits)
+        assertThat(packet).matchesPacket(
+            Packet.Operator(
+                version = 7,
+                typeId = MAXIMUM,
+                packets = listOf(
+                    Packet.Literal(
+                        version = 2,
+                        typeId = LITERAL,
+                        literal = 1L,
+                    ),
+                    Packet.Literal(
+                        version = 4,
+                        typeId = LITERAL,
+                        literal = 2L,
+                    ),
+                    Packet.Literal(
+                        version = 1,
+                        typeId = LITERAL,
+                        literal = 3L,
+                    ),
+                )
+            ),
+        )
+
+        assertThat(part1.solve(operatorInput1)).isComputed(14)
+        assertThat(part2.solve(operatorInput1)).isComputed(3)
+    }
+
+    @Test
+    fun `p1 sample - solve 1`() {
+        val input = part1.parse(listOf("8A004A801A8002F478"))
+
+        assertThat(part1.solve(input)).isComputed(16)
+        assertThat(part2.solve(input)).isComputed(15)
+    }
+
+    @Test
+    fun `p1 sample - solve 2`() {
+        val input = part1.parse(listOf("620080001611562C8802118E34"))
+
+        assertThat(part1.solve(input)).isComputed(12)
+        assertThat(part2.solve(input)).isComputed(46)
+
+        assertThat(Packet.parse(input.bits)).matchesPacket(
+            Packet.Operator(
+                version = 3,
+                typeId = SUM, // 46
+                packets = listOf(
+                    Packet.Operator(
+                        version = 0,
+                        typeId = SUM, // 21
+                        packets = listOf(
+                            Packet.Literal(
+                                version = 0,
+                                typeId = LITERAL,
+                                literal = 10L,
+                            ),
+                            Packet.Literal(
+                                version = 5,
+                                typeId = LITERAL,
+                                literal = 11L,
+                            ),
+                        ),
+                    ),
+                    Packet.Operator(
+                        version = 1,
+                        typeId = SUM, // 25
+                        packets = listOf(
+                            Packet.Literal(
+                                version = 0,
+                                typeId = LITERAL,
+                                literal = 12L,
+                            ),
+                            Packet.Literal(
+                                version = 3,
+                                typeId = LITERAL,
+                                literal = 13L,
+                            ),
+                        ),
+                    ),
+                ),
+            )
+        )
+    }
+
+    @Test
+    fun `p1 sample - solve 3`() {
+        val input = part1.parse(listOf("A0016C880162017C3686B18A3D4780"))
+
+        assertThat(part1.solve(input)).isComputed(31)
+        assertThat(part2.solve(input)).isComputed(54)
+
+        assertThat(Packet.parse(input.bits)).matchesPacket(
+            Packet.Operator(
+                version = 5,
+                typeId = SUM,
+                packets = listOf(
+                    Packet.Operator(
+                        version = 1,
+                        typeId = SUM,
+                        packets = listOf(
+                            Packet.Operator(
+                                version = 3,
+                                typeId = SUM, // 54
+                                packets = listOf(
+                                    Packet.Literal(
+                                        version = 7,
+                                        typeId = LITERAL,
+                                        literal = 6,
+                                    ),
+                                    Packet.Literal(
+                                        version = 6,
+                                        typeId = LITERAL,
+                                        literal = 6,
+                                    ),
+                                    Packet.Literal(
+                                        version = 5,
+                                        typeId = LITERAL,
+                                        literal = 12,
+                                    ),
+                                    Packet.Literal(
+                                        version = 2,
+                                        typeId = LITERAL,
+                                        literal = 15,
+                                    ),
+                                    Packet.Literal(
+                                        version = 2,
+                                        typeId = LITERAL,
+                                        literal = 15,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            )
+        )
     }
 
     @Test
@@ -44,7 +206,7 @@ class Day16SolutionTest {
         val input = part1.parseResource(INPUT_RESOURCE)
 
         val result = part1.solve(input)
-        assertThat(result).isComputed(769)
+        assertThat(result).isComputed(955)
 
         Benchmark.run(
             inputContent = loadLines(INPUT_RESOURCE),
@@ -53,11 +215,33 @@ class Day16SolutionTest {
     }
 
     @Test
-    fun `p2 - sample`() {
-        val input = part2.parseResource(SAMPLE_RESOURCE)
+    fun `p2 - sample 1`() {
+        val input = part2.parse(listOf("C0015000016115A2E0802F182340"))
+        assertThat(part2.solve(input)).isComputed(46)
+    }
 
-        val result = part2.solve(input)
-        assertThat(result).isComputed(315)
+    @Test
+    fun `p2 - sample 2`() {
+        val input = part2.parse(listOf("A0016C880162017C3686B18A3D4780"))
+        assertThat(part2.solve(input)).isComputed(54)
+    }
+
+    @Test
+    fun `p2 - samples`() {
+        val tests = mapOf(
+            "C200B40A82" to 3L,
+            "04005AC33890" to 54L,
+            "880086C3E88112" to 7L,
+            "CE00C43D881120" to 9L,
+            "D8005AC2A8F0" to 1L,
+            "F600BC2D8F" to 0L,
+            "9C005AC2F8F0" to 0L,
+            "9C0141080250320F1802104A08" to 1L,
+        )
+        assertThat(tests).allSatisfy { inputLine, expected ->
+            val input = part2.parse(listOf(inputLine))
+            assertThat(part2.solve(input)).isComputed(expected)
+        }
     }
 
     @Test
@@ -65,13 +249,54 @@ class Day16SolutionTest {
         val input = part2.parseResource(INPUT_RESOURCE)
 
         val result = part2.solve(input)
-        assertThat(result).isComputed(2963L)
+        assertThat(result).isComputed(158135423448L)
 
         Benchmark.run(
             inputContent = loadLines(INPUT_RESOURCE),
             solution = part2,
-            inputConfig = Benchmark.Stage.INPUT.config(warmupIterations = 2000U, iterations = 5_000U),
-            solveConfig = Benchmark.Stage.SOLVE.config(warmupIterations = 2000U, iterations = 5_000U),
         )
     }
+}
+
+private fun ObjectAssert<Packet.Operator>.matchesOperator(
+    expected: Packet.Operator,
+): ObjectAssert<Packet.Operator> {
+    return satisfies(Consumer { packet ->
+        assertThat(packet.version).isEqualTo(expected.version)
+        assertThat(packet.typeId).isEqualTo(expected.typeId)
+
+        assertThat(packet.packets).hasSize(expected.packets.size)
+
+        assertThat(packet.packets.zip(expected.packets)).allSatisfy(
+            Consumer { (p, e) -> assertThat(p).matchesPacket(e) }
+        )
+    })
+}
+
+private fun ObjectAssert<Packet.Literal>.matchesLiteral(
+    expected: Packet.Literal,
+): ObjectAssert<Packet.Literal> {
+    return satisfies(
+        Consumer { packet ->
+            assertThat(packet.version).isEqualTo(expected.version)
+            assertThat(packet.typeId).isEqualTo(LITERAL)
+            assertThat(packet.literal).isEqualTo(expected.literal)
+        }
+    )
+}
+
+@Suppress("UNCHECKED_CAST")
+private fun ObjectAssert<Packet>.matchesPacket(
+    expected: Packet,
+): ObjectAssert<Packet> {
+    return satisfies(
+        Consumer { packet ->
+            when (packet!!) {
+                is Packet.Operator ->
+                    (this as ObjectAssert<Packet.Operator>).matchesOperator(expected as Packet.Operator)
+                is Packet.Literal ->
+                    (this as ObjectAssert<Packet.Literal>).matchesLiteral(expected as Packet.Literal)
+            }
+        }
+    )
 }
